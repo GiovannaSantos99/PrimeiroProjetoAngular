889c8e519a8987c8e07490fe2fd7fc3092c7c442
GiovannaSantos99 <giovanna.santos99@yahoo.com>
2021-04-13 15:15:46 -0300

    Mudando title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Pagina/home/home.component';
+import { ContatoComponent } from './Pagina/contato/contato.component';
+import { ListaComponent } from './Pagina/lista/lista.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch:'full'},
+  {path: "home", component: HomeComponent},
+  {path: "lista", component: ListaComponent},
+  {path: "contato", component: ContatoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Pagina/home/home.component';
+import { ContatoComponent } from './Pagina/contato/contato.component';
+import { ListaComponent } from './Pagina/lista/lista.component';
+import { MenuComponent } from './Pagina/menu/menu.component';
+
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import ptBr from '@angular/common/locales/pt';
+registerLocaleData(ptBr);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContatoComponent,
+    ListaComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'pt'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
